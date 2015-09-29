e55ce009fbb9834dd3e4f91d08db5d825d4030ca
Brake <tropht@gmail.com>
2015-09-29 08:30:44 -0600

    finished the quiz with a lot of assistance.

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,30 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;
 
+  for (var i = 0; i < arr.length; i++){
+    var elem = arr[i];
+    if (typeof elem === 'object'){
+      arraySum(elem);
+      total = total + arraySum(elem);
+    }else {
+      total = total + elem;
+    }
+  }
+
+  return total;
+  // var total = 0;
+  // for(var i = 0; i < arr.length; i++){
+  //   if (Object.prototype.toString.call( arr[i][i] ) === '[object Array]') { //Checks to see if it an array
+  //     for(x = 0; x < arr[i].length; x++){
+  //       total = arr[i][x] + total;
+  //     }
+  //   }else{
+  //     total = arr[i] + total;
+  //   }
+  // }
+  // return total;
 }
 
 module.exports = arraySum;
